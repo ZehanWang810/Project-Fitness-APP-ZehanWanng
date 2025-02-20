18cfaae043590f023bab1e7d936e50e4d2b11005
ZehanWang810 <wangzehannb@163.com>
2025-02-20 12:17:15 -0800

    Add files via upload

---
+++ src/Record.js
@@ -0,0 +1,27 @@
+/**
+ * The Record class is a base class for different types of records.
+ * It contains common properties and methods that can be shared by subclasses.
+ * @class Record
+ */
+class Record {
+    /**
+     * Creates a Record instance.
+     * @constructor
+     * @param {number} recordId - The unique identifier of the record.
+     * @param {Date} date - The date when the record was created.
+     */
+    constructor(recordId, date) {
+        this.recordId = recordId;
+        this.date = date;
+    }
+
+    /**
+     * Gets the basic information of the record.
+     * @returns {string} - A string containing the basic information of the record.
+     */
+    getBasicInfo() {
+        return `Record ID: ${this.recordId}\nDate: ${this.date.toDateString()}`;
+    }
+}
+
+export default Record;
