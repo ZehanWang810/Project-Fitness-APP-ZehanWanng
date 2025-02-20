db8d47d6542a9b9a6fc48474b0da6c7c4395329b
ZehanWang810 <wangzehannb@163.com>
2025-02-20 12:12:59 -0800

    Add files via upload

---
+++ CommunityPost.test.js
@@ -0,0 +1,23 @@
+import CommunityPost from '../src/CommunityPost.js';
+import User from '../src/User.js';
+
+describe('CommunityPost Class', () => {
+    test('CommunityPost constructor and methods', () => {
+        const user = new User('test_user', 'password', 'test@example.com');
+        const post = new CommunityPost(user, 'Test content');
+
+        expect(post.author).toBe(user);
+        expect(post.content).toBe('Test content');
+        expect(post.createdAt instanceof Date).toBe(true);
+        expect(post.likes).toBe(0);
+        expect(post.comments.length).toBe(0);
+
+        post.addLike();
+        expect(post.likes).toBe(1);
+
+        post.addComment(user, 'Test comment');
+        expect(post.comments.length).toBe(1);
+        expect(post.comments[0].user).toBe(user);
+        expect(post.comments[0].comment).toBe('Test comment');
+    });
+});
